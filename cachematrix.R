ecfe7426d8dc63f1e88c89d86448b1f55ad4b710
tanmaysarkar <tanmay.sarkar@gmail.com>
2015-03-20 10:27:10 -0700

    Vector cache function
    Updated the file with Make Cache Matrix and Cache Solve function

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix(),...) {
+        inver <- NULL
+        set <- function(y) {
+                x <<- y
+                inver <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) inver <<- solve
+        getinverse <- function() inver
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x=matrix(), ...) {
+        inver <- x$getinverse()
+        if(!is.null(inver)) {
+                message("getting cached data")
+                return(inver)
+        }
+     	  matrix<-x$get()
+        inver <- solve(matrix, ...)
+	  x$setinverse(inver)
+        inver
 }
